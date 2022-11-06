24aba4241f2a5eeccfb29ebca10e1b7e94d5d39f
azam1919 <azamnaveed1919@gmail.com>
2022-11-06 23:39:13 +0500

    new chnagess

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,5 +32,8 @@ class AppServiceProvider extends ServiceProvider
         // if ($this->app->environment('production')) {
         //     URL::forceScheme('https');
         // }
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
