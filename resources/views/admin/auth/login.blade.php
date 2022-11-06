cac0446e592fb07583ec89681612e824b8c7dd1c
azam1919 <azamnaveed1919@gmail.com>
2022-11-06 23:50:02 +0500

    new chnaging

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -11,14 +11,14 @@
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('/assets/plugins/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{ public_path('asset/plugins/fontawesome-free/css/all.min.css')}}">
     <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="{{ asset('/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ public_path('asset/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('/assets/dist/css/adminlte.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/assets/dist/css/alt/login.css') }}">
+    <link rel="stylesheet" href="{{ public_path('asset/dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ public_path('asset/dist/css/alt/login.css') }}">
 
-    <script src="{{ asset('/assets/dist/js/pages/login.js')}}"></script>
+    <script src="{{ public_path('asset/dist/js/pages/login.js')}}"></script>
     <script>
         setTimeout(function() {
             $('#failed').slideUp('slow');
@@ -139,11 +139,11 @@
     <!-- /.login-box -->
 
     <!-- jQuery -->
-    <script src="{{ asset('/assets/plugins/jquery/jquery.min.js')}}"></script>
+    <script src="{{ public_path('asset/plugins/jquery/jquery.min.js')}}"></script>
     <!-- Bootstrap 4 -->
-    <script src="{{ asset('/assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ public_path('asset/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <!-- AdminLTE App -->
-    <script src="{{ asset('/assets/dist/js/adminlte.min.js')}}"></script>
+    <script src="{{ public_path('asset/dist/js/adminlte.min.js')}}"></script>
 </body>
 
 </html>
