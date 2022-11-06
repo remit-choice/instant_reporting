df45c5e7297f2226c72efe262e1ddd5ca2dad863
azam1919 <azamnaveed1919@gmail.com>
2022-11-06 23:55:10 +0500

    new chnaging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,8 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // $this->app->bind('path.public', function () {
+        //     return base_path() . '/public';
+        // });
         $this->app->bind('path.public', function () {
-            return base_path() . '/public';
+            return realpath(base_path() . '../public');
         });
     }
 
